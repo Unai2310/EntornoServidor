2490b8953071a7c91c3c8314f0d7965e45fef18c
Unai2310 <unaiyanton@gmail.com>
2022-10-21 18:51:19 +0200

    ForoCompleto

--- Entregas/foro_vo/app/detalles.php
+++ Entregas/foro_vo/app/detalles.php
@@ -1,10 +1,18 @@
+<?php
+include_once 'app/funciones.php';
+$sinetiqueta=quitarEtiquetas($_REQUEST['comentario']);
+
+$comentario=strtolower($sinetiqueta);
+?>
+
+
 <div>
 <b> Detalles:</b><br>
 <table>
-<tr><td>Longitud:          </td><td><?= strlen($_REQUEST['comentario']) ?></td></tr>
-<tr><td>Nº de palabras:    </td><td>6</td></tr>
-<tr><td>Letra + repetida:  </td><td>a</td></tr>
-<tr><td>Palabra + repetida:</td><td>Hola</td></tr>
+<tr><td>Longitud:          </td><td><?= strlen($comentario) ?></td></tr>
+<tr><td>Nº de palabras:    </td><td><?= str_word_count($comentario,0) ?></td></tr>
+<tr><td>Letra + repetida:  </td><td><?= cuentaLetras($comentario) ?></td></tr>
+<tr><td>Palabra + repetida:</td><td><?=cuentaPalabras($comentario) ?></td></tr>
 </table>
 </div>
 
--- Entregas/foro_vo/app/funciones.php
+++ Entregas/foro_vo/app/funciones.php
@@ -1,6 +1,36 @@
 <?php
-function usuarioOk($usuario, $contraseña) :bool {
+function usuarioOk($usuario, $contrasenia) :bool {
   
-   return ($usuario == "pepe");
-    
+  return ($usuario == strrev($contrasenia)); 
 }
+
+function quitarEtiquetas($cadena) :String {
+  return strip_tags($cadena);
+}
+
+function cuentaLetras($cadena) :String {
+  $arr = str_split($cadena);
+  $letras = [];
+  foreach ($arr as $letra) {
+    if (!array_key_exists($letra,$letras)) {
+      $letras[$letra]=0;
+    }
+    $letras[$letra]+=1;
+  }
+  arsort($letras);
+  return array_key_first($letras);
+}
+
+function cuentaPalabras($cadena) :String {
+  $partes = explode(" ", $cadena);
+  $words = [];
+  foreach ($partes as $palabra) {
+    if (!array_key_exists($palabra, $words)) {
+      $words[$palabra]=0;
+    }
+    $words[$palabra]+=1;
+  }
+  arsort($words);
+  return array_key_first($words);
+}
+?>
